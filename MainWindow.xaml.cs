741c5e151c22785aa430a4a89cbdd6dc9f29f876
GrandDynamo <43609220+GrandDynamo@users.noreply.github.com>
2020-04-21 19:08:30 +0200

    Learning to use a button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace OneDrive_Cloud_Player
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString());
+        }
     }
 }
