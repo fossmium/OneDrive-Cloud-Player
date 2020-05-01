b6e929a5b723a689b32b3d54c34e16d7318ff4d8
JohannesKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2020-05-01 14:20:09 +0200

    partially added data model classes

---
+++ DataStructure/CurrentDirectory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneDrive_Cloud_Player.DataStructure
+{
+	class CurrentDirectory
+	{
+	}
+}
---
+++ ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using OneDrive_Cloud_Player.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneDrive_Cloud_Player
+{
+	class ViewModel
+	{
+
+		private DataModel dataModel;
+
+		public DataModel DataModel {
+			get { return dataModel; }
+			set { dataModel = value; }
+		}
+
+	}
+}
