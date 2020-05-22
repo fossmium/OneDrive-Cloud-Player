58850301255b3edf6b97ad80c0e860838a4cfb45
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2020-05-22 13:36:45 +0200

    groot deel explorer gedaan

---
+++ Explorer/Explorer/Show.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Explorer
+{
+    class Show
+    {
+        public string Title { get; private set; }
+        public string Genre { get; private set; }
+
+        public Uri ImageLocation { get; private set; }
+        private int rating = 0;
+        public int Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value > 0 && value < 11)
+                {
+                    rating = value;
+                }
+            }
+        }
+
+        public Show(string Title, string Genre)
+        {
+            this.Title = Title;
+            this.Genre = Genre;
+        }
+
+        public Show(string Title, string Genre, Uri ImageLocation)
+        {
+            this.Title = Title;
+            this.Genre = Genre;
+            this.ImageLocation = ImageLocation;
+        }
+    }
+}
