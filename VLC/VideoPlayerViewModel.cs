27ba10ce920274ed17b4f302aa48cb4c15f036a0
GrandDynamo <43609220+GrandDynamo@users.noreply.github.com>
2020-05-29 00:52:31 +0200

    Many changes to media controls
    * Cursor hides after some time when you dont move it. When you move it, it shows again.
    
    * The controls panel hides after same time as cursor when you dont move the cursor.
    
    * The controls panel shows when you move your mouse and it stays open when your mouse has entered the panel. It closes again after some time when your cursor leaves the panel again.
    
    * Volume control added.
    
    * Video can be paused/played and stopped.

--- VLC/VideoPlayerViewModel.cs
+++ VLC/VideoPlayerViewModel.cs
@@ -18,25 +18,41 @@ using System.Runtime.CompilerServices;
 
 namespace OneDrive_Cloud_Player.VLC
 {
-   partial class VideoPlayerViewModel : Window
-   {
+    partial class VideoPlayerViewModel : Window
+    {
 
         private static string pausePlayButtonTitle = "PLAY";
 
-        
 
         public static string PausePlayButtonTitle
         {
             get { return pausePlayButtonTitle; }
-            set { pausePlayButtonTitle = value;
+            set
+            {
+                pausePlayButtonTitle = value;
                 //@Todo: uitzoeken waarom het hier niet werkt.
                 NotifyStaticPropertyChanged("PausePlayButtonTitle");
             }
 
         }
 
+        private static int volumeValue = 20;
 
-        public static void PauseContinueButton(LibVLC _libVLC, VideoView VideoView)
+        public static int VolumeValue
+        {
+            get { return volumeValue; }
+            set { volumeValue = value;
+                NotifyStaticPropertyChanged("VolumeValue");
+                SetVolume(value);
+                Console.WriteLine("New Volume Value!: " + volumeValue);
+            }
+        }
+
+
+        public static VideoView VideoView {get;set;}
+
+
+        public static void PauseContinueButton(LibVLC _libVLC)
         {
             Console.WriteLine("CanPause: " + VideoView.MediaPlayer.CanPause);
 
@@ -45,6 +61,9 @@ namespace OneDrive_Cloud_Player.VLC
                 VideoView.MediaPlayer.Play();
                 pausePlayButtonTitle = "PAUSE";
                 NotifyStaticPropertyChanged("PausePlayButtonTitle");
+
+                VideoView.MediaPlayer.Play();
+
             }
             else
             {
@@ -53,25 +72,75 @@ namespace OneDrive_Cloud_Player.VLC
                 NotifyStaticPropertyChanged("PausePlayButtonTitle");
             }
         }
-        
-        public static void NewVideoButton(LibVLC _libVLC, VideoView VideoView, string VideoURL)
+
+        public static async void NewVideoButton(LibVLC _libVLC, string VideoURL)
         {
             //If video is pausable play video.
             if (!VideoView.MediaPlayer.IsPlaying)
             {
+                //Sets volume on startup.
+                VideoView.MediaPlayer.Volume = VolumeValue;
+
                 VideoView.MediaPlayer.Play(new Media(_libVLC,
-                    VideoURL, FromType.FromLocation));
+                VideoURL, FromType.FromLocation));
+
+
                 pausePlayButtonTitle = "PAUSE";
                 NotifyStaticPropertyChanged("PausePlayButtonTitle");
+
+                //await VideoView.MediaPlayer.Media.Parse();
+                await VideoView.MediaPlayer.Media.Parse(MediaParseOptions.ParseNetwork);
+
+                //Print Track information.
+                Console.WriteLine("\nTracks: \n");
+                foreach (var item in VideoView.MediaPlayer.Media.Tracks)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ID:              |   " + item.Id);
+                    Console.WriteLine("Type:            |   " + item.TrackType);
+                    Console.WriteLine("Description:     |   " + item.Description);
+                    Console.WriteLine("Data:            |   " + item.Data);
+                    Console.WriteLine("Codec:           |   " + item.Codec);
+                    Console.WriteLine("Language:        |   " + item.Language);
+                    Console.WriteLine("Profile:         |   " + item.Profile);
+                    Console.WriteLine("OriginalFourcc:  |   " + item.OriginalFourcc);
+
+                }
+                Console.WriteLine("TrackS:::: " + VideoView.MediaPlayer.Media.Tracks);
             }
         }
 
-        public static void StopButton(VideoView VideoView)
+        public static void AutoStartVideo(LibVLC _libVLC, string VideoURL)
+        {
+            //If video is pausable play video.
+            if (!VideoView.MediaPlayer.IsPlaying)
+            {
+                Console.WriteLine();
+                VideoView.MediaPlayer.Play(new Media(_libVLC, VideoURL, FromType.FromLocation));
+
+
+                VideoPlayerViewModel.NewVideoButton(_libVLC, VideoURL);
+                Console.WriteLine("Hardware Decoding: " + VideoView.MediaPlayer.EnableHardwareDecoding);
+            }
+        }
+
+        private static void SetVolume(int Volume)
+        {
+            VideoView.MediaPlayer.Volume = Volume;
+        }
+
+        public static void StopButton()
         {
             if (VideoView.MediaPlayer.IsPlaying)
             {
                 VideoView.MediaPlayer.Stop();
             }
+        }
+
+        public static void DisposeVLC()
+        {
+            VideoView.MediaPlayer.Stop();
+            VideoView.Dispose();
         }
 
         public static event PropertyChangedEventHandler StaticPropertyChanged;
@@ -81,5 +150,7 @@ namespace OneDrive_Cloud_Player.VLC
         {
             StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
         }
+
+
     }
 }
--- VLC/VideoPlayerWindow.xaml.cs
+++ VLC/VideoPlayerWindow.xaml.cs
@@ -8,59 +8,144 @@ using OneDrive_Cloud_Player.VLC;
 using MediaPlayer = LibVLCSharp.Shared.MediaPlayer;
 using LibVLCSharp.WPF;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Windows.Input;
 
 namespace OneDrive_Cloud_Player.VLC
 {
-	partial class VideoPlayerWindow : Window
-	{
+    partial class VideoPlayerWindow : Window
+    {
+        private DispatcherTimer dispatcherTimer;
+        private MediaPlayer _mediaPlayer;
+        private LibVLC _libVLC;
+        private string VideoURL;
+        private bool RunDispatcher;
+        public string ButtonTitle { set; get; }
 
-		private MediaPlayer _mediaPlayer;
-		private LibVLC _libVLC;
-		private string VideoURL;
-		public string ButtonTitle { set; get; }
+        public VideoPlayerWindow(string VideoURL)
+        {
+            InitializeComponent();
+            //Create a timer with interval of 2 secs
+            dispatcherTimer = new DispatcherTimer();
+            RunDispatcher = true;
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            ButtonTitle = "TestPause";
+            this.VideoURL = VideoURL;
+            var label = new Label
+            {
+                Content = "TEwdsdasST",
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                Foreground = new SolidColorBrush(Colors.Red)
+            };
+            test.Children.Add(label);
 
-		public VideoPlayerWindow(string VideoURL)
-		{
-			InitializeComponent();
-			ButtonTitle = "TestPause";
-			this.VideoURL = VideoURL;
-			var label = new Label
-			{
-				Content = "TEwdsdasST",
-				HorizontalAlignment = HorizontalAlignment.Right,
-				VerticalAlignment = VerticalAlignment.Bottom,
-				Foreground = new SolidColorBrush(Colors.Red)
-			};
-			test.Children.Add(label);
+            Core.Initialize();
 
-			Core.Initialize();
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
 
-			_libVLC = new LibVLC();
-			_mediaPlayer = new MediaPlayer(_libVLC);
-			//we need the VideoView to be fully loaded before setting a MediaPlayer on it.
-			videoView.Loaded += (sender, e) => videoView.MediaPlayer = _mediaPlayer;
+            //we need the VideoView to be fully loaded before setting a MediaPlayer on it.
+            //videoView.Loaded += (sender, e) => videoView.MediaPlayer = _mediaPlayer;
 
-			
-		}
+            //Set videoview field int the static VideoPlayerViewModel class.
+            VideoPlayerViewModel.VideoView = videoView;
 
-		private void PauseContinueButton_Click(object sender, RoutedEventArgs e)
-		{
-			VideoPlayerViewModel.PauseContinueButton(_libVLC, videoView);
-		}
 
-		private void StopButton_Click(object sender, RoutedEventArgs e)
-		{
-			VideoPlayerViewModel.StopButton(videoView);
-		}
+            videoView.MediaPlayer = _mediaPlayer;
 
-		protected override void OnClosed(EventArgs e)
-		{
-			videoView.Dispose();
-		}
+            AutoStartVideo();
+        }
+        private void OnCaptureMouseRequest(object sender, RoutedEventArgs e)
+        {
+            Mouse.Capture(test);
+            Console.WriteLine("MoveMent");
+        }
+        private void PauseContinueButton_Click(object sender, RoutedEventArgs e)
+        {
+            VideoPlayerViewModel.PauseContinueButton(_libVLC);
+        }
 
-		private void NewVideoButton_Click(object sender, RoutedEventArgs e)
-		{
-			VideoPlayerViewModel.NewVideoButton(_libVLC, videoView, this.VideoURL);
-		}
-	}
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            VideoPlayerViewModel.StopButton();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            VideoPlayerViewModel.DisposeVLC();
+        }
+
+        private void NewVideoButton_Click(object sender, RoutedEventArgs e)
+        {
+            VideoPlayerViewModel.NewVideoButton(_libVLC, this.VideoURL);
+        }
+
+        private void AutoStartVideo()
+        {
+            VideoPlayerViewModel.AutoStartVideo(this._libVLC, this.VideoURL);
+        }
+
+       /// <summary>
+       /// Event for when the move moves. Resets the dispatcherTimer for hiding the controls.
+       /// </summary>
+       /// <param name="sender"></param>
+       /// <param name="e"></param>
+        private void VideoControls_MouseMove(object sender, MouseEventArgs e)
+        {
+            VideoControls.Visibility = Visibility.Visible;
+            //Shows cursor again.
+            Mouse.OverrideCursor = null;
+            //Resets timer if its running when you move.
+            //Console.WriteLine(Mouse.GetPosition(test));
+            if (dispatcherTimer.IsEnabled)
+            {
+                dispatcherTimer.Stop();
+            }
+            if (RunDispatcher)
+            {
+                //Start the timer
+                dispatcherTimer.Start();
+            }
+
+        }
+
+        /// <summary>
+        /// Start Counting down to hide the control elements.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            //Things which happen after 1 timer interval
+            VideoControls.Visibility = System.Windows.Visibility.Collapsed;
+
+            //Hides Cursor.
+            Mouse.OverrideCursor = Cursors.None;
+
+            //Disable the timer
+            dispatcherTimer.IsEnabled = false;
+        }
+
+        private void StopDispatcher()
+        {
+            dispatcherTimer.Stop();
+        }
+
+        private void VideoControls_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Enter");
+            RunDispatcher = false;
+            StopDispatcher();
+        }
+
+        private void VideoControls_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Leave");
+            RunDispatcher = true;
+        }
+    }
 }
