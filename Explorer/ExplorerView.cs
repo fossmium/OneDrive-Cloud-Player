8f33447b14e83785abcae31fa6427ead4fb3b6ae
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2020-05-31 15:27:31 +0200

    TempSave

--- Explorer/ExplorerView.cs
+++ Explorer/ExplorerView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OneDrive_Cloud_Player.API;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -10,34 +11,20 @@ namespace Explorer
 {
     class ExplorerView : MainWindow, INotifyPropertyChanged
     {
+
+        private Graph graph;
         List<Show> showList { get; set; }
         
         public ExplorerView()
         {
-            showList = new List<Show>();
-            CreateShowList();
-            BindData(showList);
+            this.graph = new Graph();
+            graph.GetDriveInformationAsync();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public List<Show> CreateShowList()
-        {
-            showList.Add(new Show("Attack on Titan", "Action"));
-            showList.Add(new Show("World End", "Anime"));
-            showList.Add(new Show("Twilight", "?????"));
-            showList.Add(new Show("Spiderman", "Spooder"));
-
-            return showList;
-        }
-
-        public void BindData(List<Show> showList)
-        {
-            
         }
     }
 }
