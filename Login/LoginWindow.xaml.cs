86b2fcc8a8e27eecdc69b08670d7ff9e8decac14
Remy Conen <remy.conen@hotmail.com>
2020-06-11 13:57:35 +0200

    started to merge old branch

--- Login/LoginWindow.xaml.cs
+++ Login/LoginWindow.xaml.cs
@@ -8,13 +8,9 @@ namespace OneDrive_Cloud_Player.Login
     /// <summary>
     /// Interaction logic for LoginWindows.xaml
     /// </summary>
-<<<<<<< Updated upstream
-    public partial class LoginWindows : Window
-=======
     public partial class LoginWindow : MetroWindow
->>>>>>> Stashed changes
     {
-        public LoginWindows()
+        public LoginWindow()
         {
             InitializeComponent();
         }
