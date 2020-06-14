61f989f2dd7eb7d9e787abab292bbf43b44d6e97
JohannesKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2020-06-14 16:44:47 +0200

    added cache datastructure

---
+++ Caching/GraphData/CachedDriveCollection.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OneDrive_Cloud_Player.Caching.GraphData
+{
+	/// <summary>
+	/// This class represents a cached drive. This can either be your own OneDrive folder, or a shared folder.
+	/// </summary>
+	class CachedDriveCollection
+	{
+		/// <summary>
+		/// The id of the drive, used to uniquely identify a drive
+		/// </summary>
+		[JsonProperty("driveId")]
+		public string DriveId { get; private set; }
+
+		/// <summary>
+		/// The name of the drive
+		/// </summary>
+		[JsonProperty("driveName")]
+		public string DriveName { get; private set; }
+
+		/// <summary>
+		/// The name of the owner of this drive
+		/// </summary>
+		[JsonProperty("ownerName")]
+		public string OwnerName { get; private set; }
+
+		/// <summary>
+		/// All the items stored in this drive
+		/// </summary>
+		[JsonProperty("items")]
+		public List<CachedDriveItem> ItemList { get; private set; }
+
+	}
+}
---
+++ Caching/GraphData/OneDriveCache.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OneDrive_Cloud_Player.Caching.GraphData
+{
+	class OneDriveCache
+	{
+		/// <summary>
+		/// This represents a collection of all the drives (owned or shared) of the currently signed in user.
+		/// </summary>
+		public List<CachedDriveCollection> DriveList { get; private set; }
+
+	}
+}
