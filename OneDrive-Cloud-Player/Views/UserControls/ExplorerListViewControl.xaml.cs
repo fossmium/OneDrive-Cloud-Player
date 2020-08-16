c0edab5d8251758b03669f7a14758a9b289ad8e5
Brandon <43607839+brann0n@users.noreply.github.com>
2020-08-16 17:50:55 +0200

    Made the ExplorerListViewControl working with its own bindings, using the DependencyProperty class

--- OneDrive-Cloud-Player/Views/UserControls/ExplorerListViewControl.xaml.cs
+++ OneDrive-Cloud-Player/Views/UserControls/ExplorerListViewControl.xaml.cs
@@ -1,13 +1,16 @@
 ﻿
 using CommonServiceLocator;
+using GalaSoft.MvvmLight;
 using Microsoft.Graph;
 using OneDrive_Cloud_Player.Models.GraphData;
 using OneDrive_Cloud_Player.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -23,30 +26,70 @@ using Windows.UI.Xaml.Navigation;
 
 namespace OneDrive_Cloud_Player.Views.UserControls
 {
-    public sealed partial class ExplorerListViewControl : UserControl
+    public sealed partial class ExplorerListViewControl : UserControl, INotifyPropertyChanged
     {
-
-        public List<CachedDriveItem> ExplorerItemsList
+        /// <summary>
+        /// Codebehind property that can be set from the xaml code
+        /// </summary>
+        public List<CachedDriveItem> CurrentExplorerItems
         {
-            get { return (List<CachedDriveItem>)GetValue(ExplorerItemsListProperty); }
-            set { SetValue(ExplorerItemsListProperty, value); }
+            get 
+            { 
+                return (List<CachedDriveItem>)GetValue(ExplorerItemsDependency); 
+            }
+            set 
+            {
+                SetValueDependency(ExplorerItemsDependency, value);
+            }
         }
 
-        // Using a DependencyProperty as the backing store for ExplorerItemsList.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ExplorerItemsListProperty =
-            DependencyProperty.Register("ExplorerItemsList", typeof(List<CachedDriveItem>), typeof(ExplorerListViewControl), new PropertyMetadata(0));
+        /// <summary>
+        /// Codebehind property that can be set from the xaml code
+        /// </summary>
+        public CachedDriveItem CurrentSelectedExplorerItem
+        {
+            get 
+            { 
+                return (CachedDriveItem)GetValue(SelectedExplorerItemDependency); 
+            }
+            set 
+            {
+                SetValueDependency(SelectedExplorerItemDependency, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for ExplorerItems.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ExplorerItemsDependency =
+            DependencyProperty.Register("CurrentExplorerItems", typeof(List<CachedDriveItem>), typeof(ExplorerListViewControl), null);
+
+        public static readonly DependencyProperty SelectedExplorerItemDependency =
+            DependencyProperty.Register("CurrentSelectedExplorerItem", typeof(CachedDriveItem), typeof(ExplorerListViewControl), null);
+ 
 
         public ExplorerListViewControl()
         {
             this.InitializeComponent();
+
+            //required for using this element as a bindable datacontext
+            (this.Content as FrameworkElement).DataContext = this;
         }
 
+        public event DoubleTappedEventHandler UserControlClicked;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public event EventHandler UserControlClicked;
-
-        private void ExplorerListBox_MouseDown(object sender, DoubleTappedRoutedEventArgs e)
+		private void ExplorerListBox_MouseDown(object sender, DoubleTappedRoutedEventArgs e)
         {
-            UserControlClicked?.Invoke(this, EventArgs.Empty);
+            UserControlClicked?.Invoke(this, e);         
         }
+
+        /// <summary>
+        /// Sets the value of the depenency and updates it to the view
+        /// </summary>
+        /// <param name="propertyName"></param>
+		private void SetValueDependency(DependencyProperty property, object value, [CallerMemberName] String p = null)
+        {
+            SetValue(property, value);
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
+		}
     }
 }
