b6a36bce0f46cd222c0db8a4f91cf3966ff16665
Brandon <43607839+brann0n@users.noreply.github.com>
2020-08-16 18:41:56 +0200

    Added a bindable property for choosing view type

--- OneDrive-Cloud-Player/Views/UserControls/ExplorerListViewControl.xaml.cs
+++ OneDrive-Cloud-Player/Views/UserControls/ExplorerListViewControl.xaml.cs
@@ -28,6 +28,12 @@ namespace OneDrive_Cloud_Player.Views.UserControls
 {
     public sealed partial class ExplorerListViewControl : UserControl, INotifyPropertyChanged
     {
+        public enum ViewType
+		{
+            Grid = 1000, 
+            List = 2000
+		}
+
         /// <summary>
         /// Codebehind property that can be set from the xaml code
         /// </summary>
@@ -58,13 +64,41 @@ namespace OneDrive_Cloud_Player.Views.UserControls
             }
         }
 
-        // Using a DependencyProperty as the backing store for ExplorerItems.  This enables animation, styling, binding, etc...
+        public ViewType ControlViewType
+		{
+            get
+            {
+                return (ViewType)GetValue(ControlViewTypeDependency);
+            }
+            set
+            {
+                SetValueDependency(ControlViewTypeDependency, value);
+                ViewTypeChanged(); //trigger an update with custom code
+            }
+        }
+
+
+        /// <summary>
+        /// Dependency Property for the ExplorerItems, this allows binding to it from a parent class
+        /// </summary>
         public static readonly DependencyProperty ExplorerItemsDependency =
             DependencyProperty.Register("CurrentExplorerItems", typeof(List<CachedDriveItem>), typeof(ExplorerListViewControl), null);
 
+        /// <summary>
+        /// Dependency Property for the SelectedExplorerItem, this allows binding to it from a parent class.
+        /// </summary>
         public static readonly DependencyProperty SelectedExplorerItemDependency =
             DependencyProperty.Register("CurrentSelectedExplorerItem", typeof(CachedDriveItem), typeof(ExplorerListViewControl), null);
- 
+
+        /// <summary>
+        /// Dependency Property for the SelectedExplorerItem, this allows binding to it from a parent class.
+        /// </summary>
+        public static readonly DependencyProperty ControlViewTypeDependency =
+            DependencyProperty.Register("ControlViewType", typeof(CachedDriveItem), typeof(ExplorerListViewControl), null);
+
+
+        public event DoubleTappedEventHandler UserControlClicked;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public ExplorerListViewControl()
         {
@@ -74,9 +108,11 @@ namespace OneDrive_Cloud_Player.Views.UserControls
             (this.Content as FrameworkElement).DataContext = this;
         }
 
-        public event DoubleTappedEventHandler UserControlClicked;
-		public event PropertyChangedEventHandler PropertyChanged;
-
+        /// <summary>
+        /// The event used for double clicking a list item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void ExplorerListBox_MouseDown(object sender, DoubleTappedRoutedEventArgs e)
         {
             UserControlClicked?.Invoke(this, e);         
@@ -91,5 +127,77 @@ namespace OneDrive_Cloud_Player.Views.UserControls
             SetValue(property, value);
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
 		}
-    }
+
+        /// <summary>
+        /// Handles the viewtype changed event, this function also fires the on the first load, as mvvm is slow and needs time to load its dependency values :)
+        /// </summary>
+        private void ViewTypeChanged()
+		{
+            Console.WriteLine("MainPage display type: " + ControlViewType);
+
+			switch (ControlViewType)
+			{
+				case ViewType.Grid:
+                    //enable the gridview
+                    EnableGridView();
+
+                    //disable the listview (even if this was never enabled)
+                    DisableListView();
+                    break;
+				case ViewType.List:
+                    //enable the listview
+                    EnableListView();
+
+                    //disable the gridview (even if this was never enabled)
+                    DisableGridView();
+                    break;
+			}			
+		}
+
+        private void EnableListView()
+        {
+            ExplorerListBox.Visibility = Visibility.Visible;
+
+            //create a selectedItem binding for the listbox
+            Binding selectedItemBinding = new Binding();
+            selectedItemBinding.Path = new PropertyPath("CurrentSelectedExplorerItem");
+            selectedItemBinding.Mode = BindingMode.TwoWay;
+            ExplorerListBox.SetBinding(Selector.SelectedItemProperty, selectedItemBinding);
+
+            //create a listsource binding for the listbox
+            Binding listSource = new Binding();
+            listSource.Path = new PropertyPath("CurrentExplorerItems");
+            ExplorerListBox.SetBinding(ItemsControl.ItemsSourceProperty, listSource);
+        }
+
+        private void DisableListView()
+		{
+            ExplorerListBox.Visibility = Visibility.Collapsed;
+            ExplorerListBox.ClearValue(SelectedExplorerItemDependency); //reset possible bindings
+            ExplorerListBox.ClearValue(ExplorerItemsDependency); //reset possible bindings
+        }
+
+        private void EnableGridView()
+		{
+            ExplorerGridBox.Visibility = Visibility.Visible;
+
+            //create a selectedItem binding for the listbox
+            Binding selectedItemBinding = new Binding();
+            selectedItemBinding.Path = new PropertyPath("CurrentSelectedExplorerItem");
+            selectedItemBinding.Mode = BindingMode.TwoWay;
+            ExplorerGridBox.SetBinding(Selector.SelectedItemProperty, selectedItemBinding);
+
+            //create a listsource binding for the listbox
+            Binding listSource = new Binding();
+            listSource.Path = new PropertyPath("CurrentExplorerItems");
+            ExplorerGridBox.SetBinding(ItemsControl.ItemsSourceProperty, listSource);
+        }
+
+        private void DisableGridView()
+		{
+            ExplorerGridBox.Visibility = Visibility.Collapsed;
+            ExplorerGridBox.ClearValue(SelectedExplorerItemDependency); //reset possible bindings
+            ExplorerGridBox.ClearValue(ExplorerItemsDependency); //reset possible bindings
+        }
+	}
 }
