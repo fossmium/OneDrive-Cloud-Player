f24e92e408769629c1a77e559f529805d1cd263d
JohannesKauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2020-12-24 11:32:11 +0100

    Removed even more unnecessary usings and template comments.

--- OneDrive-Cloud-Player/Models/Interfaces/INavigable.cs
+++ OneDrive-Cloud-Player/Models/Interfaces/INavigable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OneDrive_Cloud_Player.Models.Interfaces
+﻿namespace OneDrive_Cloud_Player.Models.Interfaces
 {
     public interface INavigable
     {
--- OneDrive-Cloud-Player/Views/MainPage.xaml.cs
+++ OneDrive-Cloud-Player/Views/MainPage.xaml.cs
@@ -1,28 +1,7 @@
-﻿using OneDrive_Cloud_Player.Models.GraphData;
-using OneDrive_Cloud_Player.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Windows.UI.Xaml.Controls;
 
 namespace OneDrive_Cloud_Player.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
