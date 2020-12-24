ccb3d88843c52e7b3df60cff3fb24b895431de6e
JohannesKauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2020-12-24 12:30:41 +0100

    Forgot to remove template comment.

--- OneDrive-Cloud-Player/Views/SettingsPage.xaml.cs
+++ OneDrive-Cloud-Player/Views/SettingsPage.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace OneDrive_Cloud_Player.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page
     {
 
