2df4822877152a47180f30794da191c2e4afcc28
Johannes Kauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-09-19 18:50:48 +0200

    Updated version and changelog to 1.2.2.
    Also show the version number in the dialog.

--- OneDrive-Cloud-Player/ViewModels/SettingsPageViewModel.cs
+++ OneDrive-Cloud-Player/ViewModels/SettingsPageViewModel.cs
@@ -71,15 +71,13 @@ namespace OneDrive_Cloud_Player.ViewModels
         {
             ContentDialog whatsNewDialog = new ContentDialog
             {
-                Title = $"What's new in {PackageDisplayName}",
+                Title = $"What's new in {PackageDisplayName} {AppVersion}",
                 PrimaryButtonText = "Ok",
                 DefaultButton = ContentDialogButton.Primary,
                 Background = new SolidColorBrush(Color.FromArgb(255, 30, 41, 49)),
             };
-            whatsNewDialog.Content += "* Added a settings page\n";
-            whatsNewDialog.Content += "* You can now go directly to the next or previous video from within the \n   videoplayer\n";
-            whatsNewDialog.Content += "* Added more hotkeys to the videoplayer\n";
-            whatsNewDialog.Content += "* The application is now licensed under GPLv2 only";
+            whatsNewDialog.Content += "* Fixed an issue where sometimes the audio would be heard playing when \n   it was expected to be quiet\n";
+            whatsNewDialog.Content += "* Updated several libraries and dependencies to the latest version\n";
 
             await whatsNewDialog.ShowAsync();
         }
